03135f5b8f45c9112117c72890b292f59f3735b9
Joao Macedo <joao.macedo@noshape.com>
2021-12-13 12:38:50 +0000

    v2

--- testeProjeto/Program.cs
+++ testeProjeto/Program.cs
@@ -6,7 +6,34 @@ namespace testeProjeto
 {
     class Program
     {
-        public static async Task Talk()
+
+        private static int counter = 0;
+        private static readonly object lockObject = new object();
+        public static void Increment()
+        {
+            lock (lockObject)
+            {
+                counter++;
+            }
+        }
+        public static void Decrement()
+        {
+            lock (lockObject)
+            {
+                counter--;
+            }
+        }
+        public static int Counter
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    return counter;
+                }
+            }
+        }
+        public static async Task Talk(string text)
         {
             //Definir a configuração
             var config = SpeechConfig.FromSubscription("18d7c45cbb6544ef8a7e6f08834692ff", "westeurope");
@@ -17,16 +44,20 @@ namespace testeProjeto
             using (var synthesizer = new SpeechSynthesizer(config))
             {
                 //Definir o que vai ser dito
-                Console.WriteLine("Digita o que queres que diga");
-                Console.Write("> ");
-                string text = Console.ReadLine();
+                //Console.WriteLine("Digita o que queres que diga");
+                //Console.Write("> ");
+                //string text = Console.ReadLine();
 
                 //Esperar até que tudo seja dito
                 using (var result = await synthesizer.SpeakTextAsync(text))
                 {
                     if (result.Reason == ResultReason.SynthesizingAudioCompleted)
                     {
-                        Console.WriteLine($"Speech synthesized to speaker for text [{text}]");
+                        Console.WriteLine($"Texto sintetisado para [{text}]");
+                        if(Counter == 5)
+                        {
+                            System.Environment.Exit(1);
+                        }
                         await Listen();
                     }
                     else if (result.Reason == ResultReason.Canceled)
@@ -62,16 +93,103 @@ namespace testeProjeto
                 var result2 = await recognizer.RecognizeOnceAsync();
                 if (result2.Reason == ResultReason.RecognizedSpeech)
                 {
-                    Console.WriteLine($"We recognized: {result2.Text}");
-                    if (result2.Text == "OK.")
+                    Console.WriteLine($"Reconhecemos: {result2.Text}");
+                    if (result2.Text == "OK." || result2.Text == "Sim." || result2.Text.Contains("OK."))
                     {
-                        await Talk();
+                        Increment();
+                        if (Counter == 1)
+                        {
+                            await Talk("Bloco 2");
+                        }
+                        if(Counter == 2)
+                        {
+                            await Talk("Nível 0");
+
+                        }
+                        if (Counter == 3)
+                        {
+                            await Talk("Dígito 1");
+                        }
+                        if (Counter == 4)
+                        {
+                            if (result2.Text.Contains("265"))
+                            {
+                                await Talk("265, 3 caixas");
+                            }
+                            else
+                            {
+                                Decrement();
+                                await Talk("ID inválido, 265 seria o correto");
+                            }
+                        }
+                        if(Counter == 5)
+                        {
+                            if (result2.Text.Contains("3"))
+                            {
+                                await Talk("Processo concluido!");
+                            }
+                            else
+                            {
+                                Decrement();
+                                await Talk("Número inválido, 3 caixas seria o correto");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        String repeat;
+                        if (Counter == 0)
+                        {
+                            repeat = "já está no corredor 1 ?";
+                            await Talk("Não entendi o que foi dito ," + repeat);
+                        }
+                        if (Counter == 1)
+                        {
+                            repeat = "já localizou o bloco 2?";
+                            await Talk("Não entendi o que foi dito ," + repeat);
+                        }
+                        if (Counter == 2)
+                        {
+                            repeat = "já localizou o nível 0?";
+                            await Talk("Não entendi o que foi dito ," + repeat);
+                        }
+                        if (Counter == 3)
+                        {
+                            repeat = "já localizou o dígito 1?";
+                            await Talk("Não entendi o que foi dito ," + repeat);
+                        }
+                        if (Counter == 4)
+                        {
+                            repeat = "já obteve as 3 caixas?";
+                            await Talk("Não entendi o que foi dito ," + repeat);
+                        }
+                       
                     }
 
                 }
                 else if (result2.Reason == ResultReason.NoMatch)
                 {
                     Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    if(Counter == 0)
+                    {
+                        await Talk("Já está no corredor 1?");
+                    }
+                    if(Counter == 1)
+                    {
+                        await Talk("Já localizou o bloco 2?");
+                    }
+                    if(Counter == 2)
+                    {
+                        await Talk("Já localizou o nível 0?");
+                    }
+                    if(Counter == 3)
+                    {
+                        await Talk("Já localizou o dígito 1?");
+                    }
+                    if (Counter == 4)
+                    {
+                        await Talk("Já obteve as 3 caixas?");
+                    }
                 }
                 else if (result2.Reason == ResultReason.Canceled)
                 {
@@ -87,11 +205,10 @@ namespace testeProjeto
                 }
             }
         }
-
-
+            
         static async Task Main()
         {
-            await Talk();
+            await Talk("Vá para o corredor 1");
         }
     }
 }
